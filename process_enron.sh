5293675c407075418cfe2b1b741cc3b3327eec90
shenzhun <shenzhunallen@gmail.com>
2013-08-24 18:27:46 +0800

    modify sed command

--- process_enron.sh
+++ process_enron.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 
 # concatination all files into one 
-#cat $(find . -name *.txt)
 rm -rf pre/
 
 cd ./raw/ham/
@@ -13,30 +12,24 @@ cd ../..
 filelist=$(find raw/ -type f | sed '/tar.gz/d')
 for i in $filelist
 do
+
+# get the path of cleaned files
 echo processing $i
 txt_path=$(echo $i | sed 's/raw/pre/g')
 echo $txt_path
+
+# get directory and sub directory of pre/
 dir_path=$(echo $txt_path | grep -o '.*\/')
 echo creating $dir_path
+
+# create pre/ and its sub directory
 mkdir -pv $dir_path
+
 # lupper case to lower case
 cat $i | tr '[:upper:]' '[:lower:]' | tee enron.raw.all | \
 
-# remove html tags ans some special tags
-sed -e 's/<[a-zA-Z\/][^>]*>//g' -e 's/&nbsp[;=]//g' \
-    -e 's/&nbsp//g' -e 's/#//g' -e 's/#&*;//g' -e 's/&[0-9]*//g' | \
-
-# remove blank lines
-sed -e '/^$/d' -e 's/^ *//g' -e 's/ *$//g' -e 's/^=*//g' -e '/-\{2,\}/d' | tee issue.1 | \
-
-# remove html tags between multilines 
-sed -e 'N;N;N;s/<[a-z\/]*.*>/---------------/g' -e 'N;s/<[a-z\/]*.*/---------------/g' -e 'N;s/.*>/---------------/g' | \
-
-# remove ;;;; lines
-sed -e '/;\{3,\}/d' -e '/[a-z0-9]\{20,\}/d' | \
-
-# remove duplacate tags
-sed -e 's/[-_+]\{5,\}//g' -e 's/\$\{5,\}//g' > $txt_path
+# clean html tags and store the result into pre/
+sed -f sedfile > $txt_path
 
 done
 
